9cfc9e25eb65d80c8ff8b2c42a8020ae1f6703e8
nullises <texanico@gmail.com>
2024-07-10 09:15:12 -0400

    feature/orders-microservice

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -1,10 +1,14 @@
 import 'dotenv/config';
+
 import * as joi from 'joi';
 
 interface EnvVars {
   PORT: number;
   PRODUCTS_MICROSERVICE_HOST: string;
   PRODUCTS_MICROSERVICE_PORT: number;
+
+  ORDERS_MICROSERVICE_HOST: string;
+  ORDERS_MICROSERVICE_PORT: number;
 }
 
 const envsSchema = joi
@@ -12,6 +16,9 @@ const envsSchema = joi
     PORT: joi.number().required(),
     PRODUCTS_MICROSERVICE_HOST: joi.string().required(),
     PRODUCTS_MICROSERVICE_PORT: joi.number().required(),
+
+    ORDERS_MICROSERVICE_HOST: joi.string().required(),
+    ORDERS_MICROSERVICE_PORT: joi.number().required(),
   })
   .unknown(true);
 
@@ -25,6 +32,10 @@ const envVars: EnvVars = value;
 
 export const envs = {
   port: envVars.PORT,
+
   productsMicroserviceHost: envVars.PRODUCTS_MICROSERVICE_HOST,
   productsMicroservicePort: envVars.PRODUCTS_MICROSERVICE_PORT,
+
+  ordersMicroserviceHost: envVars.ORDERS_MICROSERVICE_HOST,
+  ordersMicroservicePort: envVars.ORDERS_MICROSERVICE_PORT,
 };
